8842825bee1cecfc36a663842cd1cfab8347aced
Jack Spinola <jackspinola198@hotmail.com>
2023-11-06 18:15:00 +0100

    PFC-26 #time 2m deleted app gcd

--- Cloud/src/main/java/com/example/petfeedercloud/controllers/TestController.java
+++ Cloud/src/main/java/com/example/petfeedercloud/controllers/TestController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    private static final String template = "Hello, %s!";
-
 //http://localhost:8080/test
     @GetMapping("/test")
     public String test() {
