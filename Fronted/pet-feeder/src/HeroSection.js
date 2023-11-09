d56f3bbc1dfb6f846d65e69247f6eca972664663
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-11-09 22:08:43 +0100

    PFF-18 #time 1h 10m Start of Hero component

---
+++ Fronted/pet-feeder/src/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './HeroSection.css'; // Import the CSS file
+
+const HeroSection = () => {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Forgot to feed your pet again? Try PetFeeder!</h1>
+        <p>Automated feeding station for your best friend.</p>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
