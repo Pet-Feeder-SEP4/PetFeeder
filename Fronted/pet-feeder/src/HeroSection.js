eb2d2a4e183d630c7f7b354d4b3f8759e6b45ea1
Kristians Danilovs <325178@VIAUC.DK>
2023-11-13 14:01:31 +0100

    PFF-18 #time 1h Hero display and buttons

--- Fronted/pet-feeder/src/HeroSection.js
+++ Fronted/pet-feeder/src/HeroSection.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import './HeroSection.css'; // Import the CSS file
+import './HeroSection.css'; 
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
       <div className="hero-content">
-        <h1>Forgot to feed your pet again? Try PetFeeder!</h1>
-        <p>Automated feeding station for your best friend.</p>
+        <h1 className="display-3 ">Forgot to feed your pet again? Try PetFeeder!</h1>
+        <p className="display-6">Automated feeding station for your best friend.</p>
+        <button type="button" className="btn btn-light ">Log in</button>
+        <button type="button" className="btn btn-light ms-2 ">Sign up</button>
       </div>
     </div>
   );
