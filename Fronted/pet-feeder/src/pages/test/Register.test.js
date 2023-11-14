14c70066b69a53ae63f6de4eab404d6d52b19c4d
Jack Spinola <jackspinola198@hotmail.com>
2023-11-14 19:52:04 +0100

    merge teest

--- Fronted/pet-feeder/src/pages/test/Register.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Register from '../Register/Register';
-
-test('renders Register component without crashing', () => {
-  render(<Register />);
-});
