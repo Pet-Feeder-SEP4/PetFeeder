1f26a95bd4ef28290965a57b9cba8bee7a37d507
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-11-14 13:05:42 +0100

    PFF-18 #time 10min merge issue again

--- Fronted/pet-feeder/src/App.js
+++ Fronted/pet-feeder/src/App.js
@@ -1,26 +1,17 @@
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from "./pages/Register/Register";
 import Welcome from './Pages/Welcome/Welcome';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      
-        <Routes>
-          
-          <Route path="/Welcome" element={<Welcome />} />
-          <Route path="/Register" element={<Register />} />
-          
-        </Routes>
-        
-      
+    <Router> 
+      <Routes>
+      <Route path="/Welcome" element={<Welcome />} />
+      <Route path="/Register" element={<Register />} />
+      </Routes>
     </Router>
-    
   );
 }
 
