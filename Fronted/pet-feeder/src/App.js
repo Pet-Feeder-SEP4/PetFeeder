4120764bb287065a8fc724cc891d27ab14299f84
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-11-14 12:15:59 +0100

    PFF-18 #time 2h10min Routing, Logo+Animation

--- Fronted/pet-feeder/src/App.js
+++ Fronted/pet-feeder/src/App.js
@@ -1,12 +1,24 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Welcome from './Pages/Welcome/Welcome';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import HeroSection from './HeroSection';
 
 function App() {
   return (
-    <div className="App">
-      <HeroSection/>
-    </div>
+    <Router>
+      
+        <Routes>
+          
+          <Route path="/Welcome" element={<Welcome />} />
+          
+        </Routes>
+      
+    </Router>
+    
   );
 }
 
---
+++ Fronted/pet-feeder/src/Pages/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import HeroSection from '../../Components/HeroSection/HeroSection';
+
+const Welcome = () => {
+  console.log('Welcome component rendered');
+  return (
+    <HeroSection/>
+  );
+};
+
+export default Welcome;
