80dead82146799788c69234c11a43cd0a0b74032
Jack Spinola <jackspinola198@hotmail.com>
2023-11-14 19:25:13 +0100

    merge

--- Fronted/pet-feeder/src/pages/Register/Register.js
+++ Fronted/pet-feeder/src/pages/Register/Register.js
@@ -98,7 +98,7 @@ const Register = () => {
                 <section >
                     <p>
                         {/* replace with react router link */}
-                        <a href="#" className="link">Sign In</a>
+                        <a  className="link">Sign In</a>
                     </p>
                 </section>
             ) : (
@@ -203,7 +203,7 @@ const Register = () => {
                         Already registered?<br />
                         <span className="line">
                             {/* replace router link here!! */}
-                            <a className="link" href="#">Sign In</a>{/* replace w lpgin*/}
+                            <a className="link" >Sign In</a>{/* replace w lpgin*/}
                         </span>
                     </p>
                 </section>
