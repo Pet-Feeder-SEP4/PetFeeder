66ee8c586ee04e9604351acb464a33ee7582457b
Kristians Danilovs <325178@VIAUC.DK>
2023-11-16 15:24:43 +0100

    PFF-23 #time 10m Info component

---
+++ Fronted/pet-feeder/src/components/PetFeederInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PetFeederInfo = () => {
+  return (
+    <div>
+      <h1>Hello, I'm a React Component!</h1>
+    </div>
+  );
+};
+
+export default PetFeederInfo;
