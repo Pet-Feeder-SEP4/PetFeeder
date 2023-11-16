e3fde075bc47d5d732e4a9e051d59089f554b205
Kristians Danilovs <325178@VIAUC.DK>
2023-11-16 14:43:27 +0100

    PFF-18 #time 30m SignUp button functional

--- Fronted/pet-feeder/src/components/HeroSection.js
+++ Fronted/pet-feeder/src/components/HeroSection.js
@@ -1,6 +1,7 @@
 // HeroSection.js
 import React, { useState, useEffect } from 'react';
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
     const [isContentVisible, setContentVisibility] = useState(false);
@@ -14,8 +15,13 @@ const HeroSection = () => {
             <div className={`hero-content ${isContentVisible ? 'fadeInRight' : ''}`}>
                 <h1 className="display-5">Forgot to feed your pet again? Try PetFeeder!</h1>
                 <p className="lead">Automated feeding station for your best friend.</p>
+                <Link to="#"> {/* replace w lpgin*/}
                 <button type="button" className="btn btn-light btn-sm">Log in</button>
+                </Link>
+                <Link to="/Register">
                 <button type="button" className="btn btn-light btn-sm ms-2">Sign up</button>
+                </Link>
+                
             </div>
             <div className="logo-container">
                 <img
