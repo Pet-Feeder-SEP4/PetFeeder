cbf09e578c04ad0e1516cd3a5ee37d0bc654a9ae
Iker Sanmartin <ikersanmartinbarreiro@gmail.com>
2023-11-21 18:04:21 +0100

    Sensor class added
    Logic to handle the sensors and send the message

--- Iot/src/main.c
+++ Iot/src/main.c
@@ -7,12 +7,13 @@
 #include "servo.h"
 #include "buttons.h"
 #include "wifi.h"
+#include "sensors.h"
 
 
 int main() {
     pc_comm_init(9600, NULL);
     wifi_init();
-
+    sensor_init();
     // Use a private connection
     wifi_command_join_AP("ORBI54","fearlessbox180");
 
@@ -21,20 +22,12 @@ int main() {
 
     while (1) {
         
-        //Data format (Adjust later so it sends sensors' data)
-        int water_level = 5;
-        int food_level = 2;
-        int food_humidity = 7;
-        int water_temperature = 4;
-        int id_number = 150;
-
+        sensor_get_data();
         //String with specified data
         char dataToSend[100]; //73 constant part (strings) and 5*3 data place holders
-        snprintf(dataToSend, sizeof(dataToSend), "water level=%03d food level=%03d food humidity=%03d dwater temperature=%03d id Number=%03d\n", water_level, food_level, food_humidity, water_temperature, id_number);
-        
+        snprintf(dataToSend, sizeof(dataToSend), sensor_get_data());
         //Formated data
         wifi_command_TCP_transmit(dataToSend, strlen(dataToSend));
-        
         // Delay for 30 minutes 
         //sleep(30 * 60); // Sleep for 30 minutes
         _delay_ms(5000);
