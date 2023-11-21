e8d3a0c26c3a27cefda7c33c26aceee2f46577af
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-11-21 11:28:54 +0100

    PFF-23 #time 1h Icon change, reposiveness, colors

--- Fronted/pet-feeder/src/components/PetFeederInfo.js
+++ Fronted/pet-feeder/src/components/PetFeederInfo.js
@@ -1,63 +1,62 @@
-
 import React from 'react';
 import './PetFeederInfo.css';
 
 const PetFeederInfo = () => {
-
-  
   return (
     <div className='aboutb'>
       <div className="container pet-feeder-container">
-        <div className="col-md-6 text-center mb-4 mt-4 leftpart">
-          <img
-            src="/assets/aboutUsPhoto.jpg"
-            alt="Pet"
-            className="pet-image"
-          />
-        </div>
-        <div className="col-md-6 text-center font-weight-light rightpart">
-          <div className="textcont">
-            <h1 className=" mb-3 texttitle ">About Us</h1>
-            <p className="textcontent" >Welcome to PetFeeder, where we blend passion and innovation to revolutionize pet care.
-              At our core, we're dedicated to creating advanced pet feeders that seamlessly integrate into your lifestyle, ensuring your furry
-              friends receive the love and nourishment they deserve, even when you're away.
-
-              What sets us apart is our commitment to merging technology and design. Our intelligent, user-friendly pet feeders boast features like programmable
-              schedules and smart connectivity, empowering pet owners to provide optimal care from anywhere in the world.
-
-              We prioritize quality, safety, and sustainability, adhering to the highest industry standards. Join us at PetFeeder
-              as we redefine pet feeding with cutting-edge solutions, because your pet's well-being is our top priority.
-            </p>
+        <div className="row">
+          <div className="col-lg-6 col-md-12 text-center mb-4 mt-4">
+            <img
+              src="/assets/aboutUsPhoto.jpg"
+              alt="Pet"
+              className="pet-image"
+            />
           </div>
-          <div className='card mt-5 shadow-lg'>
-            <div className='row'>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Quality Guarantee</h5>
-                  <p class="card-text mt-3">
-                    Best Automated Pet Feeding Station 2023 Award
-                  </p>
-                  <p class="card-text">
-                    <small class="text-muted">
-                      Approved by FEDIAF
-                    </small>
-                  </p>
+          <div className="col-lg-6 col-md-12 text-center font-weight-light mb-4 mt-4">
+            <div className="textcont mt-4">
+              <h1 className="mb-3 texttitle">ABOUT US</h1>
+              <p className="textcontent lh-base">
+                What sets us apart is our dedication to innovation and quality.
+                Our pet feeders are designed with precision and care, incorporating
+                the latest technology to ensure convenience, reliability, and the well-being
+                of your furry companions. From programmable schedules to portion control,
+                we offer a range of features that simplify feeding routines while maintaining
+                optimal nutrition.
+                Moreover, we understand the importance of trust when it comes to pet care.
+                That's why our products undergo rigorous testing to guarantee their safety and functionality.
+                We stand by our commitment to reliability, ensuring that your pets are always fed on time, regardless of your schedule.
+                Ultimately, our aim is to make your life easier while fostering a healthier and happier environment
+                for your pets. With our pet feeders, you're not just investing in a product
+                you're investing in peace of mind and the well-being of your furry family members.
+              </p>
+            </div>
+            <div className='card mt-5 shadow-lg award'>
+              <div className='row'>
+                <div className="col-lg-8 col-md-12">
+                  <div className="card-body">
+                    <h5 className="card-title">Quality Guarantee</h5>
+                    <p className="card-text mt-3">
+                      Best Automated Pet Feeding Station 2023 Award
+                    </p>
+                    <p className="card-text">
+                      <small className="text-muted">Approved by FEDIAF</small>
+                    </p>
+                  </div>
+                </div>
+                <div className="col-lg-4 col-md-12 awardcont">
+                  <img
+                    src="/assets/award.png"
+                    alt="badge"
+                    className='badgeImg mt-2'
+                  />
                 </div>
               </div>
-              <div class="col-md-4">
-                <img
-                  src="/assets/badgeGreen.png"
-                  alt="badge"
-                  className='badgeImg mt-1'
-                />
-              </div>       
+            </div>
           </div>
         </div>
-          </div>
-          
       </div>
     </div>
-
   );
 };
 
