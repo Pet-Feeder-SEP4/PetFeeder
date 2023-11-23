4795a8f82b6b6f3efa993ad6947e0c7db33e0d4b
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-11-23 15:29:05 +0100

    PFF-26 #time 30h -chatbubble +quoteblock

--- Fronted/pet-feeder/src/components/Motto.js
+++ Fronted/pet-feeder/src/components/Motto.js
@@ -5,43 +5,38 @@ const Motto = () => {
         <div className='mottob'>
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-6  ">
-                        <div className="mine messages ">
-                            <div className="message last shadow-lg ">
-                                <p className='cattext'>Follow our socials</p>
-                            </div>
-                        </div>
-                        {/* chnage this to normal icons  */}
-                        <a href="link_to_facebook_profile" target="_blank" rel="noreferrer">
-                            <img
-                                src="/assets/fg.png"
-                                alt="fb"
-                                className='social'
-                            />
-                        </a>
-
-                        <a href="link_to_instagram_profile" target="_blank" rel="noreferrer">
-                            <img
-                                src="/assets/ig.png"
-                                alt="ig"
-                                className='social'
-                            />
-                        </a>
-
-                        <a href="link_to_twitter_profile" target="_blank" rel="noreferrer">
-                            <img
-                                src="/assets/twitter.png"
-                                alt="twit"
-                                className='social'
-                            />
-                        </a>
-
+                    <div className="col-lg-4 catquote ">
+                        <figure class="text-end mt-5">
+                            <blockquote class="blockquote">
+                                <p className="catquote">"No need for human if there is PetFeeder"</p>
+                            </blockquote>
+                            <figcaption class="blockquote-footes catfoot">
+                                - Cat from <cite title="horsens" className="catfoot">Horsens</cite>
+                            </figcaption>
+                        </figure>
                     </div>
-                    <div className="col-lg-6">
+                    <div className="col-lg-4 text-center">
                         <img
                             src="/assets/mottocat.png"
                             alt="mottocat"
-                            className='cat'
+                            className='img-fluid cat'
+                        />
+                    </div>
+                    <div className="col-lg-4 ">
+                    <img
+                            src="/assets/ig.png"
+                            alt="mottocat"
+                            className='img-fluid social '
+                        />
+                        <img
+                            src="/assets/fg.png"
+                            alt="mottocat"
+                            className='img-fluid social'
+                        />
+                        <img
+                            src="/assets/twitter.png"
+                            alt="mottocat"
+                            className='img-fluid social'
                         />
                     </div>
                 </div>
