fdfb17947dc514946acc67744db6e8e80e49484d
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-11-25 15:24:51 +0100

    PFF-29

--- Fronted/pet-feeder/src/components/Navbar/Navbar.js
+++ Fronted/pet-feeder/src/components/Navbar/Navbar.js
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 
+
 function NavBar() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" >
       <Container>
-        <Navbar.Brand href="#home"><img  src={`${process.env.PUBLIC_URL}/assets/PetDarkGreen.png`} alt=''  className="d-inline-block align-top img-fluid"
-            style={{ maxHeight: '90px' }}/></Navbar.Brand>
+        <Navbar.Brand href="#home"><img  src={`${process.env.PUBLIC_URL}/assets/PetBlack.png`} alt=''  className="d-inline-block align-top img-fluid"
+            style={{ maxHeight: '56px' }}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
@@ -16,7 +17,7 @@ function NavBar() {
            
           </Nav>
           <Nav>
-            <Nav.Link href="#home">Home</Nav.Link> {/* replace link */}
+            <Nav.Link style={{ fontFamily: "Poppins"}} href="#home">Home</Nav.Link> {/* replace link */}
             
           </Nav>
         </Navbar.Collapse>
