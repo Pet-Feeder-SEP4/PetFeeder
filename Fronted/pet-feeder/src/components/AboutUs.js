a0218bd3f8e54cfd142371773350e2be312f1b6b
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-11-28 19:30:29 +0100

    welcome feels better

--- Fronted/pet-feeder/src/components/AboutUs.js
+++ Fronted/pet-feeder/src/components/AboutUs.js
@@ -1,51 +1,46 @@
 import React from 'react';
-import calendar from '../../Assets/calendar.png';
-import animal from '../../Assets/animal.png';
-import Crayon from '../../Assets/crayon.png';
-import './AboutPage.css';
-
+import './AboutUs.css';
 
 const Aboutpage = () => {
-    const workInfoData = [
-      {
-        image: calendar,
-        title: "Feeding schedules",
-        text: "Simplifying pet feeding routines through meticulously designed diets.",
-      },
-      {
-        image: Crayon,
-        title: "Feeding sessions",
-        text: "At any time, through the web application, in case you want to give the pet an extra meal.",
-      },
-      {
-        image: animal,
-        title: "Multiple pets at once",
-        text: "Do not forget your multiple friends, add them with all their details to get a specific diet!",
-      },
-    ];
-    return (
-      <div className="work-section-wrapper">
-  <div className="work-section-top">
-    <h1 className="primary-heading white-text">How It Works</h1>
-    <p className="primary-text white-text">
-    With the ability to set multiple feeding schedules, specify portion sizes, and trigger extra meals manually, our system provides the utmost flexibility and customization
-    </p>
-  </div>
-  <div className="work-section-bottom">
-    {workInfoData.map((data) => (
-      <div className="work-section-info white-text" id="card-about" key={data.title}>
-        <div className="info-boxes-img-container">
-          <img src={data.image} alt="" />
-        </div>
-        <h2>{data.title}</h2>
-        <p>{data.text}</p>
+  const workInfoData = [
+    {
+      image: '/assets/calendar.png',
+      title: "Feeding schedules",
+      text: "Simplifying pet feeding routines through meticulously designed diets.",
+    },
+    {
+      image: '/assets/crayon.png',
+      title: "Feeding sessions",
+      text: "At any time, through the web application, in case you want to give the pet an extra meal.",
+    },
+    {
+      image: '/assets/animal.png',
+      title: "Multiple pets at once",
+      text: "Do not forget your multiple friends, add them with all their details to get a specific diet!",
+    },
+  ];
+
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <h1 className="primary-heading white-text">HOW IT WORKS</h1>
+        <p className="txxxt white-text">
+          With the ability to set multiple feeding schedules, specify portion sizes, and trigger extra meals manually, our system provides the utmost flexibility and customization
+        </p>
       </div>
-    ))}
-  </div>
-</div>
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info white-text" id="card-about" key={data.title}>
+            <div className="info-boxes-img-container">
+              <img src={process.env.PUBLIC_URL + data.image} alt={`Icon for ${data.title}`} />
+            </div>
+            <h2 className='titttle'>{data.title}</h2>
+            <p className='paraph'>{data.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-
-    );
-  };
-  
-  export default Aboutpage;
+export default Aboutpage;
