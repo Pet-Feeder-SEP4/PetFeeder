ea946a4acdb21ad3aa8b44726f0527f84831c1f9
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-11-30 11:46:10 +0100

    Adding LogIn Route

--- Fronted/pet-feeder/src/App.js
+++ Fronted/pet-feeder/src/App.js
@@ -4,6 +4,7 @@ import Welcome from "./pages/Welcome/Welcome";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import FeedingSchedule from "./pages/Schedule/Schedule";
+import LogIn from "./pages/LogIn/LogIn";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
       <Route path="/Welcome" element={<Welcome />} />
       <Route path="/Register" element={<Register />} />
+      <Route path="/Login" element={<LogIn />} />
       <Route path="/Schedule" element={<FeedingSchedule />} />
       </Routes>
     </Router>
