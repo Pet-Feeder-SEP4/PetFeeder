f86d6df4bb74468b978ee42e821e1e9d2388cbb4
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-12-02 19:14:04 +0100

    remove useEffect

--- Fronted/pet-feeder/src/pages/Schedule/Schedule.js
+++ Fronted/pet-feeder/src/pages/Schedule/Schedule.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import NavBar from '../../components/Navbar/Navbar';
 import { useParams } from 'react-router-dom';
 import axios from '../../api/axios';
