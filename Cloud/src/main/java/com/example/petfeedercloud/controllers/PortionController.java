91e833d945a76c1d2a85547c58177ea3e1efcfb7
Jack Spinola <jackspinola198@hotmail.com>
2023-12-02 15:38:51 +0100

    PFC-62 #time 1h error handler

--- Cloud/src/main/java/com/example/petfeedercloud/controllers/PortionController.java
+++ Cloud/src/main/java/com/example/petfeedercloud/controllers/PortionController.java
@@ -1,5 +1,6 @@
 package com.example.petfeedercloud.controllers;
 
+import com.example.petfeedercloud.dtos.GetDTOs.GetPortionDTO;
 import com.example.petfeedercloud.dtos.PortionDTO;
 import com.example.petfeedercloud.models.Portion;
 import com.example.petfeedercloud.services.*;
@@ -9,7 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.webjars.NotFoundException;
 
+import javax.sound.sampled.Port;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/portion")
@@ -19,47 +22,68 @@ public class PortionController {
     private final PortionService portionService;
 
     @GetMapping("/{portionId}")
-    public ResponseEntity<Portion> getPortionById(@PathVariable Long portionId) {
+    public ResponseEntity<?> getPortionById(@PathVariable Long portionId) {
         try {
             Portion portion = portionService.getPortionById(portionId);
-            return ResponseEntity.ok(portion);
+            GetPortionDTO portionDTO = new GetPortionDTO(portionId,portion.getLabel(),portion.getPortionSize(),portion.getTime().getTimeId());
+            return ResponseEntity.ok(portionDTO);
         } catch (NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
     @GetMapping("/times/{timeId}")
-    public ResponseEntity<List<Portion>> getPortionsByTime(@PathVariable Long timeId) {
-        // Error handling for TimeNotFoundException goes here, if needed
-        List<Portion> portions = portionService.getPortionsByTime(timeId);
-        return ResponseEntity.ok(portions);
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> createPortion(@RequestBody PortionDTO portionDTO) {
+    public ResponseEntity<List<GetPortionDTO>> getPortionsByTime(@PathVariable Long timeId) {
         try {
-            portionService.createPortion(portionDTO);
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            List<Portion> portions = portionService.getPortionsByTime(timeId);
+            List<GetPortionDTO> portionDTOs = portions.stream()
+                    .map(portion -> new GetPortionDTO(
+                            portion.getPortionId(),
+                            portion.getLabel(),
+                            portion.getPortionSize(),
+                            portion.getTime().getTimeId()
+                    ))
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(portionDTOs);
         } catch (NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
+    @PostMapping
+    public ResponseEntity<GetPortionDTO> createPortion(@RequestBody PortionDTO portionDTO) {
+        try {
+            Portion createdPortion = portionService.createPortion(portionDTO);
+
+            GetPortionDTO createdPortionDTO = new GetPortionDTO(
+                    createdPortion.getPortionId(),
+                    createdPortion.getLabel(),
+                    createdPortion.getPortionSize(),
+                    createdPortion.getTime().getTimeId()
+            );
+
+            return new ResponseEntity<>(createdPortionDTO, HttpStatus.CREATED);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
     @PutMapping("/{portionId}")
-    public ResponseEntity<Portion> updatePortion(@PathVariable Long portionId, @RequestBody PortionDTO portionDTO) {
+    public ResponseEntity<?> updatePortion(@PathVariable Long portionId, @RequestBody PortionDTO portionDTO) {
         try {
             Portion updatedPortion = portionService.updatePortion(portionId, portionDTO);
-            return ResponseEntity.ok(updatedPortion);
+            GetPortionDTO portionDTOReturn = new GetPortionDTO(portionId,updatedPortion.getLabel(),updatedPortion.getPortionSize(),updatedPortion.getTime().getTimeId());
+            return ResponseEntity.ok(portionDTOReturn);
         } catch (NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
     @DeleteMapping("/{portionId}")
-    public ResponseEntity<Portion> deletePortion(@PathVariable Long portionId) {
+    public ResponseEntity<?> deletePortion(@PathVariable Long portionId) {
         try {
             Portion deletedPortion = portionService.deletePortion(portionId);
-            return ResponseEntity.ok(deletedPortion);
+            return new ResponseEntity<>("Portion with id "+portionId+" deleted! xD", HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (NotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
