ba8bfd9679f66149214c9e3143f8d012072335a8
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-12-03 15:51:56 +0100

    button styling

--- Fronted/pet-feeder/src/pages/MainPage/MainPage.js
+++ Fronted/pet-feeder/src/pages/MainPage/MainPage.js
@@ -3,24 +3,27 @@ import PetFeedersC from '../../components/PetFeedersC/PetFeedersC';
 import MyPets from '../../components/MyPets/MyPets';
 import NavBar from '../../components/Navbar/Navbar';
 import { Link } from 'react-router-dom';
+import './MainPage.css'; // Import the CSS file for styling
 
 const MainPage = () => {
   return (
     <>
       <NavBar />
       <div className="MainContainer">
-        <div className='buttonsinMain '>
+        {/* Buttons with absolute positioning */}
+        <div className="buttonsinMain">
           <Link to={`/putActivationPageLink/`}>
-          <button type="button" className="btnAddPetFeeder">
-            + PETFEEDER
-          </button>
+            <button type="button"  className="btn btn-success" id="btnAdd">
+              + Pet Feeder
+            </button>
           </Link>
           <Link to={`/CreatePet/`}>
-          <button type="button" className="btnAddPet">
-            + PET
-          </button>
+            <button type="button" className="btn btn-success" id="btnAdd">
+              + Pet
+            </button>
           </Link>
         </div>
+
         <div className="row">
           <div className="Feeder-column col-lg-9">
             <PetFeedersC />
