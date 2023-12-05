31420d2ee87bf00479417c3890e82c6d660497aa
Santiago Gomez <santimiguelgomez@gmail.com>
2023-12-05 19:15:08 +0100

    Data for sensors formatted to JSON

--- Iot/src/Sensors/sensors.c
+++ Iot/src/Sensors/sensors.c
@@ -6,11 +6,13 @@
 #include <dht11.h>
 #include <stdio.h>
 #include "hc_sr04.h"
+#include <stdlib.h>  // for malloc
 #include <util/delay.h>
 
 uint8_t humidity_integer, humidity_decimal, temperature_integer, temperature_decimal;
 uint16_t measure;
-char str[64];
+char str[256];  // Increase the buffer size for JSON
+//char humidTemp[256];
 int temperature;
 int humidity;
 char* waterMeasurement;
@@ -25,22 +27,23 @@ char* intToString(int value);
 int getWaterMeasurement();
 int getFoodMeasurement();
 
+void sensor_init() {
+    //pc_comm_init(9600, NULL);  // Initialize communication at the beginning
 
-void sensor_init(){
     dht11_init();
     hc_sr04_init();
-    //hc_sr04_food_init();
-    idNumber=88888888;
+    idNumber = 8888;  // Set the petFeederId to 1
 }
 
-char* sensor_get_data(){
-    pc_comm_init(9600, NULL);
-    //pc_comm_send_string_blocking("sensor class called\n");
+char* sensor_get_data() {
     getTempandHum();
-    waterMeasurement=intToString(getWaterMeasurement());
-    foodMeasurement=intToString(getFoodMeasurement());
-    sprintf(str, "water= %s food= %s   %d%d%d \n\n",
-                waterMeasurement,foodMeasurement,humidity,temperature,idNumber);
+    waterMeasurement = intToString(getWaterMeasurement());
+    foodMeasurement = intToString(getFoodMeasurement());
+
+    // Create JSON string
+    sprintf(str, "{\"petFeederId\":\"%d\",\"foodLevel\":\"%s\",\"foodHumidity\":\"%d\",\"waterTemperature\":\"%d\",\"waterLevel\":\"%s\"}\n",
+            idNumber, foodMeasurement, humidity, temperature, waterMeasurement);
+
     pc_comm_send_string_blocking(str);
     return str;
 }
@@ -48,11 +51,11 @@ char* sensor_get_data(){
 void getTempandHum(){
     // Read data from DHT11 sensor
     DHT11_ERROR_MESSAGE_t result = dht11_get(&humidity_integer, &humidity_decimal, &temperature_integer, &temperature_decimal);
-    pc_comm_init(9600, NULL);
+    //pc_comm_init(9600, NULL);
     temperature=temperature_integer;
     humidity=humidity_integer;
-    sprintf(str,"humidity:%d temperature:%d \n\n",
-            humidity,temperature);
+    //sprintf(humidTemp,"humidity:%d temperature:%d \n\n",
+            //humidity,temperature);
     pc_comm_send_string_blocking(str);
 }
 
