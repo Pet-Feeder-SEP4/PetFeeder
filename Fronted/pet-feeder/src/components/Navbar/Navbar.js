51f9f8fa95ce6a51463137f9d432ee0ee571100d
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-12-10 13:40:00 +0100

    error fix

--- Fronted/pet-feeder/src/components/Navbar/Navbar.js
+++ Fronted/pet-feeder/src/components/Navbar/Navbar.js
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
 import axios from '../../api/axios';
 
 function NavBar() {
