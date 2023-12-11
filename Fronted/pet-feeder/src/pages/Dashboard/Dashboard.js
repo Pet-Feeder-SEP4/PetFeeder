d1f660dda8dd29c3ea62f9ba494095e1d1873830
Kristians Danilovs <325178@VIAUC.DK>
2023-12-11 13:30:48 +0100

    PFF-43 #time 2h SOCKETS FIX

--- Fronted/pet-feeder/src/pages/Dashboard/Dashboard.js
+++ Fronted/pet-feeder/src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,38 @@
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SideBar from '../../components/SideBar/SideBar';
 import NavBar from '../../components/Navbar/Navbar';
 import DispensePop from '../../components/SideBar/DispensePop';
 import axios from "../../api/axios";
-import { SocketProvider } from '../../api/SocketContext';
+
 
 const Dashboard = () => {
   const { petFeederId } = useParams();
   const [isPopupVisible, setPopupVisible] = useState(false);
   const token = localStorage.getItem('token');
+  //for sockets
+  const [petFeederData, setPetFeederData] = useState(null);
+  useEffect(() => {
+    const socket = new WebSocket('wss://petfeederapi.azurewebsites.net/petfeeder/info/' + petFeederId);
+    //krissy my pissy
+    socket.addEventListener('open', (event) => {
+      console.log('WebSocket connection opened:', event);
+    });
+
+    socket.addEventListener('message', (event) => {
+      const data = JSON.parse(event.data);
+      setPetFeederData(data);
+    });
+    //krissy pissy make suree you call this if server crashes or something goes wrong 
+    socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:', event);
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, [petFeederId]);
 
   const handleDispenseClick = () => {
     setPopupVisible(true);
@@ -47,28 +69,33 @@ const Dashboard = () => {
     }
   };
 
-
-
   return (
     <>
-      <SocketProvider petFeederId={petFeederId}>
+      
         <NavBar />
         {isPopupVisible && (
           <DispensePop onClose={handleClosePopup} onDispense={handleDispense} />
         )}
-        
         <div className='dash'>
           <div className='row dashrow'>
             <div className='col-2 sideCol'>
               <SideBar onDispenseClick={handleDispenseClick} />
             </div>
             <div className='col-10'>
-
+              {petFeederData && (
+                <div>
+                  <h2>PetFeederDetailss</h2>
+                  <p>PetFeederId: {petFeederData.petFeederId}</p>
+                  <p>PetFeederLabel: {petFeederData.petFeederLabel}</p>
+                  <p>FooodLevel: {petFeederData.foodLevel}</p>
+                  <p>FoodHumidity: {petFeederData.foodHumidity}</p>
+                  <p>WaterTempserature: {petFeederData.waterTemperture}</p>
+                </div>
+              )}
             </div>
           </div>
-
         </div>
-      </SocketProvider>
+     
     </>
   );
 };
