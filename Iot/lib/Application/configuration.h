ce2f107a7b481126f7c0fc2753195dfb14454e51
f.depalma <325587@via.dk>
2023-12-11 15:12:05 +0100

    updated with IoT_test changes

---
+++ Iot/lib/Application/configuration.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define WIFI_NAME "Redmi 9C NFC"
+#define WIFI_PASSWORD "santi32411"
+#define IP "172.20.10.2"
+#define PORT 23
---
+++ Iot/lib/Application/include_avr_util.h
@@ -0,0 +1,5 @@
+#ifdef WINDOWS_TEST
+#include "mock_delay.h"
+#else
+#include <util/delay.h>
+#endif
