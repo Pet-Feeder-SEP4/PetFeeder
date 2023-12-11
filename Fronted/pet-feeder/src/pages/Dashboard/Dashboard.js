ccaa65fb4efe17e0434eb3ac8debd3ae7990e7f8
Kristians Danilovs <k.danilovs2905@gmail.com>
2023-12-11 17:12:39 +0100

    PFF-43 #time 30h Sockets, waterT comp

--- Fronted/pet-feeder/src/pages/Dashboard/Dashboard.js
+++ Fronted/pet-feeder/src/pages/Dashboard/Dashboard.js
@@ -1,21 +1,20 @@
-
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SideBar from '../../components/SideBar/SideBar';
 import NavBar from '../../components/Navbar/Navbar';
 import DispensePop from '../../components/SideBar/DispensePop';
 import axios from "../../api/axios";
-
+import WaterTemp from '../../components/WaterTemp/WaterTemp';
 
 const Dashboard = () => {
   const { petFeederId } = useParams();
   const [isPopupVisible, setPopupVisible] = useState(false);
   const token = localStorage.getItem('token');
-  //for sockets
+
   const [petFeederData, setPetFeederData] = useState(null);
   useEffect(() => {
     const socket = new WebSocket('wss://petfeederapi.azurewebsites.net/petfeeder/info/' + petFeederId);
-    //krissy my pissy
+
     socket.addEventListener('open', (event) => {
       console.log('WebSocket connection opened:', event);
     });
@@ -24,7 +23,7 @@ const Dashboard = () => {
       const data = JSON.parse(event.data);
       setPetFeederData(data);
     });
-    //krissy pissy make suree you call this if server crashes or something goes wrong 
+
     socket.addEventListener('close', (event) => {
       console.log('WebSocket connection closed:', event);
     });
@@ -71,31 +70,34 @@ const Dashboard = () => {
 
   return (
     <>
-      
-        <NavBar />
-        {isPopupVisible && (
-          <DispensePop onClose={handleClosePopup} onDispense={handleDispense} />
-        )}
-        <div className='dash'>
-          <div className='row dashrow'>
-            <div className='col-2 sideCol'>
-              <SideBar onDispenseClick={handleDispenseClick} />
-            </div>
-            <div className='col-10'>
-              {petFeederData && (
-                <div>
-                  <h2>PetFeederDetailss</h2>
-                  <p>PetFeederId: {petFeederData.petFeederId}</p>
-                  <p>PetFeederLabel: {petFeederData.petFeederLabel}</p>
-                  <p>FooodLevel: {petFeederData.foodLevel}</p>
-                  <p>FoodHumidity: {petFeederData.foodHumidity}</p>
-                  <p>WaterTempserature: {petFeederData.waterTemperture}</p>
+      <NavBar />
+      {isPopupVisible && (
+        <DispensePop onClose={handleClosePopup} onDispense={handleDispense} />
+      )}
+      <div className='dash'>
+        <div className='row dashrow'>
+          <div className='col-2 sideCol'>
+            <SideBar onDispenseClick={handleDispenseClick} />
+          </div>
+          <div className='col-10'>
+            <div class="container-upper mt-5">
+              <div class="row upper">
+                <div class="col-4 waterTemp ">
+                  <WaterTemp petFeederData={petFeederData} />
                 </div>
-              )}
+                <div class="col-4">
+                  
+                </div>
+                <div class="col-4">
+                  
+                
+                </div>
+              </div>
             </div>
           </div>
         </div>
-     
+      </div>
+
     </>
   );
 };
