912ceffd1b8ba9e5f0023ad716f55d59cd42bde5
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-12-12 13:49:04 +0100

    modals

--- Fronted/pet-feeder/src/components/Navbar/Navbar.js
+++ Fronted/pet-feeder/src/components/Navbar/Navbar.js
@@ -1,39 +1,41 @@
-import '../Navbar/Navbar.css'
-import React, { useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import axios from '../../api/axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
-import NotificationModal from '../modals/NotificationModal';
+import "../Navbar/Navbar.css";
+import React, { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import axios from "../../api/axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import NotificationModal from "../modals//Notifications/NotificationModal";
 
 function NavBar() {
   const [loggingOut, setLoggingOut] = useState(false);
   const [showNotification, setShowNotification] = useState(false);
   const [notificationCount, setNotificationCount] = useState(0);
 
-  const userId = localStorage.getItem('userId');
+  const userId = localStorage.getItem("userId");
 
   //for sockets
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    const socket = new WebSocket('wss://petfeederapi.azurewebsites.net/notificationsWS/' + userId);
+    const socket = new WebSocket(
+      "wss://petfeederapi.azurewebsites.net/notificationsWS/" + userId
+    );
 
-    socket.addEventListener('open', (event) => {
-      console.log('WebSocket connection opened:', event);
+    socket.addEventListener("open", (event) => {
+      console.log("WebSocket connection opened:", event);
     });
 
-    socket.addEventListener('message', (event) => {
+    socket.addEventListener("message", (event) => {
       const data = event.data;
       setNotifications((prevNotifications) => [data, ...prevNotifications]);
       setNotificationCount((prevCount) => prevCount + 1);
-      console.log('caralho', data);
+      console.log("caralho", data);
     });
 
-    socket.addEventListener('close', (event) => {
-      console.log('WebSocket connection closed:', event);
+    socket.addEventListener("close", (event) => {
+      console.log("WebSocket connection closed:", event);
     });
 
     return () => {
@@ -44,11 +46,11 @@ function NavBar() {
   const handleLogout = async () => {
     try {
       setLoggingOut(true);
-      const response = await axios.post('/auth/logout');
-      console.log('Logout Response:', response.data);
-      window.location.href = '/';
+      const response = await axios.post("/auth/logout");
+      console.log("Logout Response:", response.data);
+      window.location.href = "/";
     } catch (error) {
-      console.error('Error logging out:', error);
+      console.error("Error logging out:", error);
     } finally {
       setLoggingOut(false);
     }
@@ -72,42 +74,54 @@ function NavBar() {
               src={`${process.env.PUBLIC_URL}/assets/PetBlack.png`}
               alt=""
               className="d-inline-block align-top img-fluid"
-              style={{ maxHeight: '56px' }}
+              style={{ maxHeight: "56px" }}
             />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">{/* Add additional navigation items here if needed */}</Nav>
+            <Nav className="me-auto">
+              {/* Add additional navigation items here if needed */}
+            </Nav>
             <Nav>
-              <Nav.Link style={{ fontFamily: 'Poppins' }} href="/MainPage/">
+              <Nav.Link style={{ fontFamily: "Poppins" }} href="/MainPage/">
                 Home
               </Nav.Link>
               <Nav.Link
-                style={{ fontFamily: 'Poppins' }}
+                style={{ fontFamily: "Poppins" }}
                 onClick={handleLogout}
                 disabled={loggingOut}
               >
                 Logout
               </Nav.Link>
               <Nav.Link
-  style={{ fontFamily: 'Poppins' }}
-  onClick={handleNotificationClick}
-  disabled={loggingOut}
->
-  <div style={{ position: 'relative', display: 'inline-block' }}>
-    <FontAwesomeIcon style={{fontSize: '20px'}} icon={faBell} className="me-2" />
-    {notificationCount > 0 && (
-      <span className="notification-count">{notificationCount}</span>
-    )}
-  </div>
-</Nav.Link>
+                style={{ fontFamily: "Poppins" }}
+                onClick={handleNotificationClick}
+                disabled={loggingOut}
+              >
+                <div style={{ position: "relative", display: "inline-block" }}>
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={faBell}
+                    className="me-2"
+                  />
+                  {notificationCount > 0 && (
+                    <span className="notification-count">
+                      {notificationCount}
+                    </span>
+                  )}
+                </div>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
       {/* Notification Modal */}
-      <NotificationModal show={showNotification} handleClose={handleCloseNotification} notifications={notifications} />
+      <NotificationModal
+        show={showNotification}
+        handleClose={handleCloseNotification}
+        notifications={notifications}
+      />
     </>
   );
 }
--- Fronted/pet-feeder/src/components/SideBar/SideBar.js
+++ Fronted/pet-feeder/src/components/SideBar/SideBar.js
@@ -1,38 +1,72 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock, faBox, faPaw, faHistory, faFilePen } from '@fortawesome/free-solid-svg-icons';
-import './SideBar.css';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faClock,
+  faBox,
+  faPaw,
+  faHistory,
+  faFilePen,
+} from "@fortawesome/free-solid-svg-icons";
+import "./SideBar.css";
+import { useParams } from "react-router-dom";
 
 function SideBar({ onDispenseClick, onEditClick }) {
-    const { petFeederId } = useParams();
-    return (
-        <div className="sidebar ">
-            <ul className="list-group">
-                <Link to={`/schedule/${petFeederId}`} className="list-group-item mt-4" style={{ backgroundColor: "#F8F9FA" }}>
-                    <FontAwesomeIcon icon={faClock} className='me-2' />
-                    <span className="d-none d-xl-inline">SCHEDULE</span>
-                </Link>
-                <li className="list-group-item " style={{ backgroundColor: "#F8F9FA" }}>
-                    <FontAwesomeIcon icon={faBox} className='me-2' />
-                    <span className="d-none d-xl-inline" onClick={onDispenseClick}>DISPENSE</span>
-                </li>
-                <Link to="/" className="list-group-item " style={{ backgroundColor: "#F8F9FA" }}> {/* add correct path */}
-                    <FontAwesomeIcon icon={faPaw} className='me-2' />
-                    <span className="d-none d-xl-inline">PET</span>
-                </Link>
-                <Link to="/" className="list-group-item " style={{ backgroundColor: "#F8F9FA" }}> {/* add correct path */}
-                    <FontAwesomeIcon icon={faHistory} className='me-2' />
-                    <span className="d-none d-xl-inline">HISTORY</span>
-                </Link>
-                <li className="list-group-item " style={{ backgroundColor: "#F8F9FA" }}>
-                    <FontAwesomeIcon icon={faFilePen} className='me-2' />
-                    <span className="d-none d-xl-inline" onClick={onEditClick}>NOTIFICATIONS</span>
-                </li>
-            </ul>
-        </div>
-    );
+  const { petFeederId } = useParams();
+  return (
+    <div className="sidebar ">
+      <ul className="list-group">
+        <Link
+          to={`/schedule/${petFeederId}`}
+          className="list-group-item mt-4"
+          style={{ backgroundColor: "#F8F9FA" }}
+        >
+          <FontAwesomeIcon icon={faClock} className="me-2" />
+          <span className="d-none d-xl-inline">SCHEDULE</span>
+        </Link>
+        <li className="list-group-item " style={{ backgroundColor: "#F8F9FA" }}>
+          <FontAwesomeIcon
+            icon={faBox}
+            className="me-2"
+            onClick={onDispenseClick}
+          />
+          <span className="d-none d-xl-inline" onClick={onDispenseClick}>
+            DISPENSE
+          </span>
+        </li>
+        <Link
+          to="/"
+          className="list-group-item "
+          style={{ backgroundColor: "#F8F9FA" }}
+        >
+          {" "}
+          {/* add correct path */}
+          <FontAwesomeIcon icon={faPaw} className="me-2" />
+          <span className="d-none d-xl-inline">PET</span>
+        </Link>
+        <Link
+          to="/"
+          className="list-group-item "
+          style={{ backgroundColor: "#F8F9FA" }}
+        >
+          {" "}
+          {/* add correct path */}
+          <FontAwesomeIcon icon={faHistory} className="me-2" />
+          <span className="d-none d-xl-inline">HISTORY</span>
+        </Link>
+        <li className="list-group-item " style={{ backgroundColor: "#F8F9FA" }}>
+          <FontAwesomeIcon
+            icon={faFilePen}
+            className="me-2"
+            onClick={onEditClick}
+          />
+          <span className="d-none d-xl-inline" onClick={onEditClick}>
+            NOTIFICATIONS
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SideBar;
