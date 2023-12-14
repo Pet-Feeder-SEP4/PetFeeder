cbae4702341bbc37d493146fbde1ec68fc31827a
Iker Sanmartin <ikersanmartinbarreiro@gmail.com>
2023-12-14 19:36:18 +0100

    Update sensor_controller.h

--- Iot/lib/Control/sensor_controller.h
+++ Iot/lib/Control/sensor_controller.h
@@ -1,11 +1,6 @@
 #pragma once
 #include <stdint.h>
 
-extern int temperature;
-extern int humidity;
-extern int waterMeasurement;
-extern int foodMeasurement;
-extern int idNumber;
 #ifdef WINDOWS_TEST
 extern int humidity_integer, humidity_decimal, temperature_integer, temperature_decimal;
 #else
