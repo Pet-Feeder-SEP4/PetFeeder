fc0321975197c301bf7b1f64288bc46fd0de9151
martaescmadeira <100835165+martaescmadeira@users.noreply.github.com>
2023-12-14 11:00:52 +0100

    test fix

--- Fronted/pet-feeder/src/pages/Schedule/Time/Time.test.js
+++ Fronted/pet-feeder/src/pages/Schedule/Time/Time.test.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-import axios from '../../../api/axios'; // Import axios for mocking
-import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import axios from "../../../api/axios"; // Import axios for mocking
+import { MemoryRouter, useHistory } from "react-router-dom"; // Import MemoryRouter and useHistory
 
-// Import your component
-import Time from './Time';
+// Mock react-router-dom
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"), // Use actual implementation for other exports
+  useHistory: jest.fn(),
+}));
 
 // Mock axios requests
-jest.mock('../../../api/axios');
+jest.mock("../../../api/axios");
 
 // Sample test
-describe('Time Component', () => {
+describe("Time Component", () => {
   // Clean up the mock after the test
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  test('renders component and handles add button click', async () => {
+  test("renders component and handles add button click", async () => {
+    // Mock useHistory
+    const mockHistory = { push: jest.fn() };
+    useHistory.mockReturnValue(mockHistory);
+
     // Mock axios get request
-    axios.get.mockResolvedValue({ data: [{ timeId: 1, timeLabel: 'Mock Time', time: '12:00', portionSize: 2 }] });
+    axios.get.mockResolvedValue({
+      data: [
+        { timeId: 1, timeLabel: "Mock Time", time: "12:00", portionSize: 2 },
+      ],
+    });
 
     // Mock axios post request
-    axios.post.mockResolvedValue({ data: 'Mock response' });
+    axios.post.mockResolvedValue({ data: "Mock response" });
 
     // Render the component with MemoryRouter
     render(
@@ -31,23 +42,24 @@ describe('Time Component', () => {
     );
 
     // Access the input fields
-    const timeLabelInput = screen.getByLabelText('Time Label:');
-    const timeInput = screen.getByLabelText('Time:');
-    const portionSizeInput = screen.getByLabelText('Portion Size:');
+    const timeLabelInput = screen.getByLabelText("Time Label:");
+    const timeInput = screen.getByLabelText("Time:");
+    const portionSizeInput = screen.getByLabelText("Portion Size:");
 
     // Fill in input fields
-    fireEvent.change(timeLabelInput, { target: { value: 'Mock Time Label' } });
-    fireEvent.change(timeInput, { target: { value: '12:00' } });
-    fireEvent.change(portionSizeInput, { target: { value: '2' } });
+    fireEvent.change(timeLabelInput, { target: { value: "Mock Time Label" } });
+    fireEvent.change(timeInput, { target: { value: "12:00" } });
+    fireEvent.change(portionSizeInput, { target: { value: "2" } });
 
     // Click the add button
-    fireEvent.click(screen.getByText('Add'));
+    fireEvent.click(screen.getByText("Add"));
 
     // Wait for the axios request to be resolved
     await waitFor(() => expect(axios.post).toHaveBeenCalledTimes(1));
 
-    // Additional assertions can be added based on the component's behavior
+    // Check if useHistory.push was called with the expected path
+    expect(mockHistory.push).toHaveBeenCalledWith("/expected-path");
 
-   
+    // Additional assertions can be added based on the component's behavior
   });
 });
